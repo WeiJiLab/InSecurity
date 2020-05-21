32621c1e3b69364034b6a259b4b0177a31a807dd
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-05-21 21:59:10 +0800

    [XingfengYang] #N/A

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -34,14 +34,15 @@ class Header extends Component {
                     <Col md={2}> </Col>
                     <Col md={8}>
                         <Navbar bg="#fff" variant="light">
-                            <Link to="/home"><Image src={Logo} className="logo-img" rounded/>
-                                <Navbar.Brand className="icon" href="#home">
-                                    <p style={{padding: 0, margin: 0}}>TWOWASP</p>
-                                    {/*<span style={{fontWeight:'bolder',color:'#900C3F'}}>O</span>*/}
-                                    {/*<span style={{fontWeight:'bolder',color:'#C70039'}}>W</span>*/}
-                                    {/*<span style={{fontWeight:'bolder',color:'#FF5733'}}>A</span>*/}
-                                    {/*<span style={{fontWeight:'bolder',color:'#FFC300'}}>S</span>*/}
-                                    {/*<span style={{fontWeight:'bolder',color:'#DAF7A6'}}>P</span>*/}
+                            <Link to="/home" style={{fontWeight:'bolder'}}>
+                                <Image src={Logo} className="logo-img" rounded/>
+                                <Navbar.Brand className="icon-header" href="#home">
+                                    <strong className={"headerTitleSpan"}>TWOWASP</strong>
+                                    {/*<strong className={"headerTitleSpan"} style={{color:'#900C3F'}}>O</strong>*/}
+                                    {/*<strong className={"headerTitleSpan"} style={{color:'#C70039'}}>W</strong>*/}
+                                    {/*<strong className={"headerTitleSpan"} style={{color:'#FF5733'}}>A</strong>*/}
+                                    {/*<strong className={"headerTitleSpan"} style={{color:'#FFC300'}}>S</strong>*/}
+                                    {/*<strong className={"headerTitleSpan"} style={{color:'#DAF7A6'}}>P</strong>*/}
                                 </Navbar.Brand>
                             </Link>
 
