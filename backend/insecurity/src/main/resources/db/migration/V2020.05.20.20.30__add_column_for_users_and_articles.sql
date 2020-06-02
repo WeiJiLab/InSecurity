45607beb2985593c350b4785ede24cb48288e382
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-06-02 17:02:07 +0800

    [XingfengYang] #N/A

--- backend/insecurity/src/main/resources/db/migration/V2020.05.20.20.30__add_column_for_users_and_articles.sql
+++ backend/insecurity/src/main/resources/db/migration/V2020.05.20.20.30__add_column_for_users_and_articles.sql
@@ -1,2 +1,4 @@
 alter table insecurity.user add column del tinyint default 0;
 alter table insecurity.article add column hot tinyint default 0;
+
+update table insecurity.user set del = 1 where uid = 3;
