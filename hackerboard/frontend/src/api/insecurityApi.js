33a7b2ab784e61cdb57d1a3647418ea19aa5fcb2
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-06-08 16:07:41 +0800

    [XingfengYang] #N/A remove footer

--- hackerboard/frontend/src/api/insecurityApi.js
+++ hackerboard/frontend/src/api/insecurityApi.js
@@ -1,4 +1,4 @@
-const API_BASE = process.env.API_BASE || 'http://localhost:8088/';
+const API_BASE = process.env.API_BASE || "http://47.108.156.22:8088/"
 
 export const API_COOKIES = API_BASE + 'cookie/all';
 
