3306482a2f109f1b8be96cfc629b4a1969374e40
Xingfeng Yang <xingfeng.yang@thoughtworks.com>
2020-06-08 17:16:26 +0800

    [XingfengYang] #N/A add a id

--- insecurity/frontend/src/components/qrcode/QRCode.js
+++ insecurity/frontend/src/components/qrcode/QRCode.js
@@ -14,7 +14,7 @@ class QRCode extends Component {
         return (
             <Container style={{background: '#fff', padding: '1em', boxShadow: '0 1px 3px rgba(27,95,160,.1)'}}>
                 <Row>
-                    <Col md={5}> <Image style={{width:'100%'}} src={QrCode}/></Col>
+                    <Col md={5}> <Image id={"wechat-img"} style={{width:'100%'}} src={QrCode}/></Col>
                     <Col md={7} style={{paddingTop:'1.5em'}}>
                         <Row><h5>关注我们的公众号</h5></Row>
                         <Row><p>致力于传播信息安全</p></Row>
