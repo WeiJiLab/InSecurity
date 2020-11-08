4675776237f6d0df2c1e2157ca53b2e1be527f5c
NeoYang <nerosoft@outlook.com>
2020-11-08 15:55:40 +0800

    [XingfengYang] #N/A: fix a style problem

--- insecurity/frontend/src/components/menu/Menu.js
+++ insecurity/frontend/src/components/menu/Menu.js
@@ -21,25 +21,33 @@ class Menu extends Component {
         const topics = this.props.topicsResult.topics;
         return (
             <Container className="Menu">
-                <Row>
-                    <Col>
+                <Row style={{padding:0,margin:0}}>
+                    <Col style={{padding:0,margin:0}}>
                         <Navbar bg="#fff" variant="light">
-                            <Nav className="mr-auto">
-                                {this.renderTopics()}
-                                {topics && topics.length > 10 ?
-                                    <Navbar.Collapse className="justify-content-end" style={{position: 'absolute', right: '0'}}>
-                                        <Dropdown style={{float: 'right'}}>
-                                            <Dropdown.Toggle variant="success" id="dropdown-basic"
-                                                             style={{background: 'transparent', color: '#4a4a4a', border: 'none'}}>
-                                                {this.props.t('More')}
-                                            </Dropdown.Toggle>
-                                            <Dropdown.Menu>
-                                                {this.renderDropDownTags()}
-                                            </Dropdown.Menu>
-                                        </Dropdown>
-                                    </Navbar.Collapse> : null
-                                }
-                            </Nav>
+                            <Row style={{padding:0,margin:0}}>
+                                <Col md={10} style={{overflowX:'hidden',padding:0,margin:0}}>
+                                    <Nav className="mr-auto">
+                                        {this.renderTopics()}
+                                    </Nav>
+                                </Col>
+                                <Col md={2} style={{padding:0,margin:0,textAlign:'left'}}>
+                                    <Nav className="mr-auto">
+                                        {topics && topics.length > 10 ?
+                                            <Navbar.Collapse className="justify-content-end" style={{position: 'absolute', right: '0'}}>
+                                                <Dropdown style={{float: 'right'}}>
+                                                    <Dropdown.Toggle variant="success" id="dropdown-basic"
+                                                                     style={{background: 'transparent', color: '#4a4a4a', border: 'none'}}>
+                                                        {this.props.t('More')}
+                                                    </Dropdown.Toggle>
+                                                    <Dropdown.Menu>
+                                                        {this.renderDropDownTags()}
+                                                    </Dropdown.Menu>
+                                                </Dropdown>
+                                            </Navbar.Collapse> : null
+                                        }
+                                    </Nav>
+                                </Col>
+                            </Row>
                         </Navbar>
                     </Col>
                 </Row>
@@ -50,7 +58,7 @@ class Menu extends Component {
     renderDropDownTags() {
         const topics = this.props.topicsResult.topics;
         const result = [];
-        if(topics) {
+        if (topics) {
             if (topics.length > 10) {
                 for (let i = 10; i < topics.length; i++) {
                     result.push(topics[i]);
@@ -80,7 +88,7 @@ class Menu extends Component {
     renderTopics() {
         const topics = this.props.topicsResult.topics;
         const result = [];
-        if(topics) {
+        if (topics) {
             if (topics.length < 10) {
                 for (let i = 0; i < topics.length; i++) {
                     result.push(topics[i]);
@@ -94,7 +102,11 @@ class Menu extends Component {
 
         return (<Fragment>
                 <Nav.Link className="nav-tag" style={{color: "#000"}}><Link
-                    style={{color: this.props.selected ? '#4a4a4a' : 'rgb(75,166,127)',fontWeight: this.props.selected ? 'normal' : 'bold', paddingRight: '2em'}}
+                    style={{
+                        color: this.props.selected ? '#4a4a4a' : 'rgb(75,166,127)',
+                        fontWeight: this.props.selected ? 'normal' : 'bold',
+                        paddingRight: '0.5em'
+                    }}
                     to={"/"}>{this.props.t('Homepage')}</Link></Nav.Link>
                 {
                     result.map((item, index) => {
@@ -110,7 +122,7 @@ class Menu extends Component {
                             color = 'rgb(75,166,127)';
                             bold = 'bold';
                         }
-                        return <Nav.Link style={{color: "#000", paddingRight: '2em'}}>
+                        return <Nav.Link style={{color: "#000", paddingRight: '1em'}}>
                             <Link style={{color: color, fontWeight: bold}} to={path}>{item}</Link>
                         </Nav.Link>
                     })
