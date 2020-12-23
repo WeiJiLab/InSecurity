3acdfc59cf3dafbae417b1705c4c2165e56a3671
Hao Li <hayatelee@gmail.com>
2020-12-23 10:03:54 +0800

    adjust lgtm.yml

--- build_backend.sh
+++ build_backend.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash -xe
+#!/usr/bin/env bash
+
+set -xe
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "${DIR}/insecurity/backend"
 
 ./gradlew build -x test
-
 
